9da6360ba94957855dcd8a21100313d679a6ccaa
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-03-24 18:49:09 +0800

    Update Tentang testing

--- spec/controllers/tentang_controller_spec.rb
+++ spec/controllers/tentang_controller_spec.rb
@@ -4,21 +4,21 @@ RSpec.describe TentangController, type: :controller do
 
   describe "GET #visi-misi-kantor" do
     it "returns http success" do
-      get :visi-misi-kantor
+      get :visi_misi_kantor
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #tekat-urakat" do
     it "returns http success" do
-      get :tekat-urakat
+      get :tekat_urakat
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #tekat-pendakat" do
     it "returns http success" do
-      get :tekat-pendakat
+      get :tekat_pendakat
       expect(response).to have_http_status(:success)
     end
   end
