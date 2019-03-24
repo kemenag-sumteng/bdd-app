ead5d0ca5e07197ea704a3f7d4cfa22a59697c11
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-03-24 18:40:21 +0800

    Update Tentang views, controller, and navigation

--- app/controllers/tentang_controller.rb
+++ app/controllers/tentang_controller.rb
@@ -1,10 +1,13 @@
 class TentangController < ApplicationController
-  def visi-misi-kantor
+  def visi_misi_kantor
+  	@visi_misi_kantor = "Visi dan Misi Kantor Kementerian Agama Kabupaten Sumba Tengah"
   end
 
-  def tekat-urakat
+  def tekat_urakat
+  	@tekat_urakat = "Tekat URAKAT Kantor Kementerian Agama Kabupaten Sumba Tengah"
   end
 
-  def tekat-pendakat
+  def tekat_pendakat
+  	@tekat_pendakat = "Tekat PENDAKAT Kantor Kementerian Agama Kabupaten Sumba Tengah"
   end
 end
