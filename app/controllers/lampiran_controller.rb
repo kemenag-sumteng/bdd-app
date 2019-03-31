7f8c3fbf48c312e6c4e939434a367aafa9690cfa
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-03-31 17:15:27 +0800

    Unable to add Lampiran to InformasiBeritaTerkini

--- app/controllers/lampiran_controller.rb
+++ app/controllers/lampiran_controller.rb
@@ -1,2 +1,11 @@
 class LampiranController < ApplicationController
+	before_action :find_informasi_berita_terkini
+	def create
+		@lampiran = @informasi_berita_terkini.lampiran.create(params[:lampiran],permit(:tautan))
+		@lampiran.save
+	end
+	private
+	def find_informasi_berita_terkini
+		@informasi_berita_terkini = InformasiBeritaTerkini.find(params[:informasi_berita_terkini_id])
+	end
 end
