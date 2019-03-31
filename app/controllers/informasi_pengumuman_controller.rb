5b2ddefafb5934f6872a12b596befd7bb5e9be27
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-03-31 18:55:04 +0800

    Add Lampiran to InformasiPengumuman: - Add to InformasiPengumuman - Migrate

--- app/controllers/informasi_pengumuman_controller.rb
+++ app/controllers/informasi_pengumuman_controller.rb
@@ -69,6 +69,6 @@ class InformasiPengumumanController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def informasi_pengumuman_params
-      params.require(:informasi_pengumuman).permit(:judul, :isi)
+      params.require(:informasi_pengumuman).permit(:judul, :isi, :lampiran)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_31_091912) do
+ActiveRecord::Schema.define(version: 2019_03_31_103643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2019_03_31_091912) do
     t.text "isi"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "lampiran"
   end
 
   create_table "laporan_kinerja_pegawai_bimkat_sumteng", force: :cascade do |t|
