75e269612c2bc5885b6695d1479ad1d2775c7337
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-03-31 10:55:30 +0800

    Enable IFrame support

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module BddDevApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Allow IFrame support
+    config.action_dispatch.default_headers = {
+    	'X-Frame-Options' => 'ALLOWALL'
+    }
   end
 end
