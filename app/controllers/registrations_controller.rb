c385a12a470b9bfc76f4380c0c5072f093517df1
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-04-03 15:51:31 +0800

    Add nama and update views

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,14 +3,14 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit( :nama, 
+    params.require(:pemakai).permit( :nama, 
                                   :email, 
                                   :password, 
                                   :password_confirmation)
   end
 
   def account_update_params
-    params.require(:user).permit( :nama, 
+    params.require(:pemakai).permit( :nama, 
                                   :email, 
                                   :password, 
                                   :password_confirmation, 
