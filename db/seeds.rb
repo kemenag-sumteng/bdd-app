4805600f18679a99d05f22120096f7eaf8297ec7
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-04-10 23:12:30 +0800

    update seed: - Create fungsi - Create pemakai

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts ""
+puts "Seed database"
+puts ""
+Fungsi.create!([
+	{nama: "Pegawai Pendakat"},
+	{nama: "Guru Pendakat"},
+	{nama: "Penyelenggara Pendakat"},
+	{nama: "Pegawai Pendakat"},
+	{nama: "Penyuluh Pendakat"},
+	{nama: "Kasie Pendakat"},
+	{nama: "Admin"}
+])
+
+puts "Berhasil membuat #{Fungsi.count} fungsi"
+
+Pemakai.create!([
+	{email: "pegawai_pendakat@kemenagsumteng.go.id", password: "qwerty", password_confirmation: "qwerty", fungsi_id: 1},
+	{email: "guru_pendakat@kemenagsumteng.go.id", password: "qwerty", password_confirmation: "qwerty", fungsi_id: 2},
+	{email: "penyelenggara_pendakat@kemenagsumteng.go.id", password: "qwerty", password_confirmation: "qwerty", fungsi_id: 3},
+	{email: "pegawai_urakat@kemenagsumteng.go.id", password: "qwerty", password_confirmation: "qwerty", fungsi_id: 4},
+	{email: "penyuluh_urakat@kemenagsumteng.go.id", password: "qwerty", password_confirmation: "qwerty", fungsi_id: 5},
+	{email: "kasie_urakat@kemenagsumteng.go.id", password: "qwerty", password_confirmation: "qwerty", fungsi_id: 6},
+	{email: "admin@kemenagsumteng.go.id", password: "qwerty", password_confirmation: "qwerty", fungsi_id: 7}
+])
+
+puts "Berhasil membuat #{Pemakai.count} pemakai"
+
+
+
+puts ""
+puts "Proses seed database selesai"
