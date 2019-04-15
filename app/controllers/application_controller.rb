d7c29c8c7936202d476f6a2898bc19f37a637dc0
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-04-16 00:45:50 +0800

    Update CanCanCan setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::Base
 	helper_method :current_user
 	
 	rescue_from CanCan::AccessDenied do |exception|
-    render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
-    ## to avoid deprecation warnings with Rails 3.2.x (and incidentally using Ruby 1.9.3 hash syntax)
-    ## this render call should be:
-    # render file: "#{Rails.root}/public/403", formats: [:html], status: 403, layout: false
-  end
+		render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
+		## to avoid deprecation warnings with Rails 3.2.x (and incidentally using Ruby 1.9.3 hash syntax)
+		## this render call should be:
+		# render file: "#{Rails.root}/public/403", formats: [:html], status: 403, layout: false
+	 end
 end
